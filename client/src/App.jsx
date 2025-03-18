132bc4f8bb8f88032eb23bbe0708beecdd35982e
JkPendon <130830073+Jkpendon17@users.noreply.github.com>
2025-03-18 11:56:40 +0800

    Changes in app.jsx
    Updated codes in app jsx (for navigational menu)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SignupPage from "./pages/sign_up";
-import LoginPage from "./pages/loginPage";
+import LoginPage from "./pages/loginPage"; 
 import HomePage from "./pages/home_page";
+import Categories from "./pages/categories";
+import Farmers from "./pages/categories";
+import Menu from "./pages/menu";
+import Profile from "./pages/profile";
+
 
 function App() {
   return (
     <Router>
+      
       <Routes>
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/homepage" element={<HomePage />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home_page" element={<HomePage />} />
+        <Route path="/categories" element={<Categories/>} />
+        <Route path="/farmers" element={<Farmers/>} />
+        <Route path="/menu" element={<Menu/>} />
+        <Route path="/profile" element={<Profile/>} />
       </Routes>
     </Router>
   );
