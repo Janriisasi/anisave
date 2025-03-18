282ef492a0a85fe1d079fa4896047f130ba245d5
Sasi <johnreysasi55@gmail.com>
2025-03-18 16:49:16 +0800

    additional files

--- client/src/pages/productCard.jsx
+++ client/src/pages/productCard.jsx
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import "./productCard.css";
-
-
-function ProductCard({ name, Qty, marketData,productImage }) {
-  return (
-    
-    <div className="product-card">
-      <img src={productImage} alt= "tomato" className="product-image" />
-      <div className="name-color">
-      <p>{name}</p>
-      </div>
-
-      <div className="sizes">
-      <p className="small">Small</p>
-      <p className="medium">Medium</p>
-      <p className="large">Large</p>
-      </div>
-
-      <div className="qty-date"> 
-      <p className="qty">Qty: {Qty}</p>
-
-      </div>
-      <div className="market-data"> 
-      <button className="button-market" onClick={marketData}>View Market Data</button>
-      </div>
-      </div>
-    
-  );
-}
-
-=======
 import React from "react";
 import "./productCard.css";
 
@@ -61,5 +28,4 @@ function ProductCard({ name, qty, size, marketData, productImage }) {
   );
 }
 
->>>>>>> f81776cad555027c19cca363906e2647099b505d
 export default ProductCard;
