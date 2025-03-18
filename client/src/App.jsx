e2dd6ddc455dac1efd31c09063b106a2bb0c9cae
JkPendon <130830073+Jkpendon17@users.noreply.github.com>
2025-03-18 18:28:56 +0800

    Add files via upload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/loginPage"; 
 import HomePage from "./pages/home_page";
 import Categories from "./pages/categories";
-import Farmers from "./pages/categories";
+import Farmers from "./pages/farmers";
 import Menu from "./pages/menu";
 import Profile from "./pages/profile";
 
@@ -10,10 +10,9 @@ import Profile from "./pages/profile";
 function App() {
   return (
     <Router>
-      
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/homepage" element={<HomePage />} />
+        <Route path="/home_page" element={<HomePage />} />
         <Route path="/categories" element={<Categories/>} />
         <Route path="/farmers" element={<Farmers/>} />
         <Route path="/menu" element={<Menu/>} />
