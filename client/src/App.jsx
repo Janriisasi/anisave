f33d2f3228886b420c1b515f59860660ee1b9dd4
JkPendon <130830073+Jkpendon17@users.noreply.github.com>
2025-03-25 00:29:25 +0800

    Changes of route in infoProduct and profNav
    Added new Path Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/loginPage"; 
+import LoginPage from "./pages/loginPage";
 import HomePage from "./pages/home_page";
 import Categories from "./pages/categories";
 import Farmers from "./pages/farmers";
 import ProfileNav from "./pages/profileNav";
-import CustomerSupport from "./pages/customerSup";
-import AboutUs from "./pages/aboutUS";
-
-
+import Vegetable from "./category/Vegetables";
+import Fruit from "./category/Fruits";
+import Grain from "./category/Grains";
+import Herb from "./category/Herbs";
+import InfoProduct from "./infoProduct/infoProduct";
+import SellingProduct from "./pages/sellingProduct";
 
 function App() {
   return (
@@ -15,9 +17,16 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/home_page" element={<HomePage />} />
-        <Route path="/categories" element={<Categories/>} />
-        <Route path="/farmers" element={<Farmers/>} />
-        <Route path="/profileNav" element={<ProfileNav/>} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/farmers" element={<Farmers />} />
+        <Route path="/profileNav" element={<ProfileNav />} />
+        <Route path="/profileNav/:farmer_NameInfo" element={<ProfileNav />} />
+        <Route path="/vegetables" element={<Vegetable />} />
+        <Route path="/Fruits" element={<Fruit />} />
+        <Route path="/Grains" element={<Grain />} />
+        <Route path="/Herbs" element={<Herb />} />
+        <Route path="/sellingProduct" element={<SellingProduct/>} />
+        <Route path="/infoProduct/:vegetableName" element={<InfoProduct />} />
       </Routes>
     </Router>
   );
